ca7729671e527833ed71dd4e31a531445550295c
mya-24 <87232931+mya-24@users.noreply.github.com>
2022-11-05 20:07:40 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
-  end
-
-  def new
+    @posts = Post.order(id: "desc")
   end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
